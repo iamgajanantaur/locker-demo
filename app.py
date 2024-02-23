9071ea5c519af91a2f8bec07339f293e0b2ec782
Gajanan Taur <gajanan.taur96@gmail.com>
2024-02-23 10:53:56 +0530

    make required changes

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 def read_locker_data():
     locker_data = []
-    with open('./data.csv', mode='r') as file:
+    with open('./student_allocation_data/data.csv', mode='r') as file:
         csv_reader = csv.DictReader(file)
         for row in csv_reader:
             locker_data.append(row)
@@ -14,7 +14,7 @@ def read_locker_data():
 @app.route('/')
 def index():
     locker_data = read_locker_data()
-    return render_template('index.html', locker_data=locker_data)
+    return render_template('ditiss.html', locker_data=locker_data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5000, host="0.0.0.0")
