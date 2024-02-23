4c764beb6faddd7b72b027b9585cd49907e3b1c9
Gajanan Taur <gajanan.taur96@gmail.com>
2024-02-23 11:14:59 +0530

    all coursed data added

--- app.py
+++ app.py
@@ -19,6 +19,32 @@ def read_dac_data():
             locker_data.append(row)
     return locker_data
 
+def read_desd_data():
+    locker_data = []
+    with open('./student_allocation_data/desd.csv', mode='r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            locker_data.append(row)
+    return locker_data
+
+
+def read_dmc_data():
+    locker_data = []
+    with open('./student_allocation_data/dmc.csv', mode='r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            locker_data.append(row)
+    return locker_data
+
+def read_dbda_data():
+    locker_data = []
+    with open('./student_allocation_data/dbda.csv', mode='r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            locker_data.append(row)
+    return locker_data
+
+
 @app.route('/ditiss')
 def ditiss():
     locker_data = read_ditiss_data()
@@ -28,6 +54,21 @@ def ditiss():
 def dac():
     locker_data = read_dac_data()
     return render_template('dac.html', locker_data=locker_data)
+
+@app.route('/desd')
+def desd():
+    locker_data = read_dac_data()
+    return render_template('desd.html', locker_data=locker_data)
+
+@app.route('/dmc')
+def dmc():
+    locker_data = read_dac_data()
+    return render_template('dmc.html', locker_data=locker_data)
+
+@app.route('/dbda')
+def dbda():
+    locker_data = read_dac_data()
+    return render_template('dbda.html', locker_data=locker_data)
 
 if __name__ == '__main__':
     app.run(port=5000, host="0.0.0.0")
