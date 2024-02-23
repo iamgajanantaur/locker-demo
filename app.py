3b9a65a9264446d59810ff67e1edf67ad1b35dd8
Gajanan Taur <gajanan.taur96@gmail.com>
2024-02-23 11:10:28 +0530

    added dac student data

--- app.py
+++ app.py
@@ -3,18 +3,31 @@ import csv
 
 app = Flask(__name__)
 
-def read_locker_data():
+def read_ditiss_data():
     locker_data = []
-    with open('./student_allocation_data/data.csv', mode='r') as file:
+    with open('./student_allocation_data/ditiss.csv', mode='r') as file:
         csv_reader = csv.DictReader(file)
         for row in csv_reader:
             locker_data.append(row)
     return locker_data
 
-@app.route('/')
-def index():
-    locker_data = read_locker_data()
+def read_dac_data():
+    locker_data = []
+    with open('./student_allocation_data/dac.csv', mode='r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            locker_data.append(row)
+    return locker_data
+
+@app.route('/ditiss')
+def ditiss():
+    locker_data = read_ditiss_data()
     return render_template('ditiss.html', locker_data=locker_data)
+
+@app.route('/dac')
+def dac():
+    locker_data = read_dac_data()
+    return render_template('dac.html', locker_data=locker_data)
 
 if __name__ == '__main__':
     app.run(port=5000, host="0.0.0.0")
